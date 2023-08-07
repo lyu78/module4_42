fd1569432f018d48a6ea2df61b5feaf278aa46c0
Иван Иванович <lyubimkow@gmail.com>
2023-08-07 23:20:05 +0300

    Обновлен файл README.

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
 
 
 class Advertisement(models.Model):
@@ -24,6 +26,23 @@ class Advertisement(models.Model):
 
     # Дата изменения
     updated_at = models.DateTimeField(auto_now=True)
+
+    @admin.display(description="Дата создания")
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.strftime("%H:%M:%S")
+            return format_html(
+                "<span style='color:green; font-weight:bold;'> Сегодня в {}</span>",
+                created_time
+            )
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    def __str__(self):
+        return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
+
+    class Meta:
+        db_table = 'advertisements'
 
     # Изображение
 
