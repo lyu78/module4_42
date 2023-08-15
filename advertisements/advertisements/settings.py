adad392e80980eab5100c5fdce1b7203fd6ba957
Иван Иванович <lyubimkow@gmail.com>
2023-08-15 19:45:26 +0300

    Спидран лекции №9 + ДЗ лекции 9.

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'static')]
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Advertisement(models.Model):
 
+    user = models.ForeignKey(
+        User,
+        verbose_name="Пользователь",
+        on_delete=models.CASCADE
+    )
+
+    image = models.ImageField(
+        "Изображение",
+        upload_to="advertisements/"
+    )
     # Заголовок
     # Небольшое текстовое поле
     title = models.CharField("Заголовок", max_length=128)
@@ -37,6 +50,14 @@ class Advertisement(models.Model):
                 created_time
             )
         return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    @admin.display(description='Фото')
+    def get_html_image(self):
+        if self.image:
+            return format_html(
+                '<img src="{}" style="max-height:80px; max-width:80px">',
+                self.image.url
+            )
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
